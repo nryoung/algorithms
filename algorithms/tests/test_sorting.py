76f6dfdd552ff230ba08b483219e1edbf4cd1234
nryoung <nryoung@gmail.com>
2012-08-24 15:54:14 -0600

    Revert "added merge_sort tests and merge_sort"
    This reverts commit 958835581a623abc7bd66f69fa182abbdd89df5e.

--- algorithms/tests/test_sorting.py
+++ algorithms/tests/test_sorting.py
@@ -1,6 +1,6 @@
 import random
 import unittest
-from ..sorting import bubble_sort, selection_sort, insertion_sort, merge_sort
+from ..sorting import bubble_sort, selection_sort, insertion_sort
 
 class TestBubbleSort(unittest.TestCase):
     """
@@ -28,27 +28,9 @@ class TestInsertionSort(unittest.TestCase):
     """
     Tests Insertion sort on a small range from 0-9
     """
-    def test_insertionsort(self):
+    def test_selectionsort(self):
         self.seq = range(10)
         random.shuffle(self.seq)
         rv = insertion_sort.sort(self.seq)
         self.assertIs(self.seq[0], 0)
         self.assertIs(self.seq[-1], 9)
-
-class TestMergeSort(unittest.TestCase):
-    """
-    Tests Merge sort on a small range from 0-9
-    """
-    def test_mergesort(self):
-        self.seq = range(10)
-        random.shuffle(self.seq)
-        rv = merge_sort.sort(self.seq)
-        self.assertIs(rv[0], 0)
-        self.assertIs(rv[-1], 9)
-
-    def test_merge(self):
-        self.seq1 = range(5)
-        self.seq2 = range(5,10)
-        rv = merge_sort.merge(self.seq1, self.seq2)
-        self.assertIs(rv[0], 0)
-        self.assertIs(rv[-1], 9)
--- venv/lib/python2.7/site-packages/distribute-0.6.27-py2.7.egg/setuptools/script template (dev).py
+++
@@ -1,6 +0,0 @@
-# EASY-INSTALL-DEV-SCRIPT: %(spec)r,%(script_name)r
-__requires__ = """%(spec)r"""
-from pkg_resources import require; require("""%(spec)r""")
-del require
-__file__ = """%(dev_path)r"""
-execfile(__file__)
