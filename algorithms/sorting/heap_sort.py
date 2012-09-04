f6678fcc6ecfc02b7f8023c8e61787de785e0141
Nic Young <nryoung@gmail.com>
2012-09-03 23:12:54 -0600

    heap_sort.py now sorts correctly.

--- algorithms/sorting/heap_sort.py
+++ algorithms/sorting/heap_sort.py
@@ -1,19 +1,10 @@
 """ Implementation of Heap Sort """
 
-def parent(i):
-    return i/2
-
-def left(i):
-    return 2*i
-
-def right(i):
-    return 2*i+1
-
 def max_heapify(seq, i, n):
-    l = left(i)
-    r = right(i)
+    l = i + 1
+    r = i + 2
 
-    if l <= n and seq[n] > seq[i]:
+    if l <= n and seq[l] > seq[i]:
         largest = l
     else:
         largest = i
@@ -24,20 +15,18 @@ def max_heapify(seq, i, n):
         seq[i], seq[largest] = seq[largest], seq[i]
         max_heapify(seq, largest, n)
 
-def heap_length(seq):
-    return len(seq) - 1
 
 def build_heap(seq):
-    n = heap_length(seq)
-    for i in range(n/2,0,-1):
+    n = len(seq) - 1
+    for i in range(n, -1, -1):
         max_heapify(seq, i, n)
 
 def sort(seq):
     build_heap(seq)
-    heap_size = heap_length(seq)
-    for i in range(heap_size,1,-1):
-        seq[1], seq[i] = seq[i], seq[1]
+    heap_size = len(seq) - 1
+    for x in range(heap_size, -1, -1):
+        seq[0], seq[x] = seq[x], seq[0]
         heap_size = heap_size - 1
-        max_heapify(seq, 1, heap_size)
+        max_heapify(seq, 0, heap_size)
     
     return seq
