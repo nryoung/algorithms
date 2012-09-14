b88147464cd8d20aa1aa4e8ead42d988b38eec9b
nryoung <nryoung@gmail.com>
2012-09-14 15:51:20 -0600

    Fixed binary_search and binary_search tests

--- algorithms/searching/binary_search.py
+++ algorithms/searching/binary_search.py
@@ -1,18 +1,20 @@
 """ Implementation of Binary Search """
 
 def search(seq, key):
+    imin = 0
+    imax = len(seq) - 1
     
-    if len(seq) == 0:
+    if imax < imin :
         return False
 
     else:
-        mid = len(seq)/2
+        mid = len(seq) / 2
 
         if seq[mid] > key:
-            return search(seq[:mid], key)
+            return search(seq[:mid-1], key)
 
         elif seq[mid] < key:
-            return search(seq[mid:], key)
+            return search(seq[mid+1:], key)
 
         else:
             return True
--- algorithms/tests/test_searching.py
+++ algorithms/tests/test_searching.py
@@ -8,7 +8,11 @@ class TestBinarySearch(unittest.TestCase):
     """
     def test_binarysearch(self):
         self.seq = range(10)
-        rv1 = binary_search.search(self.seq, 8)
-        rv2 = binary_search.search(self.seq, 3)
+        rv1 = binary_search.search(self.seq, 0)
+        rv2 = binary_search.search(self.seq, 9)
+        rv3 = binary_search.search(self.seq, -1)
+        rv4 = binary_search.search(self.seq, 10)
         self.assertTrue(rv1)
         self.assertTrue(rv2)
+        self.assertFalse(rv3)
+        self.assertFalse(rv4)
