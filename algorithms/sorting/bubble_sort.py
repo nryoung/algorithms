0de7c420b9db1a43266f3916af6b4083d27ac4bb
Neil <kngspook@gmail.com>
2012-09-23 16:33:45 -0700

    No need to use temp until a swap actually needs to occur (approximately a 22% speedup).

--- algorithms/sorting/bubble_sort.py
+++ algorithms/sorting/bubble_sort.py
@@ -25,12 +25,10 @@
 
 
 def sort(seq):
-
     for x in seq:
-        temp = 0
         for n in range(1, len(seq)):
-            temp = seq[n]
             if seq[n] < seq[n - 1]:
+                temp = seq[n]
                 seq[n] = seq[n - 1]
                 seq[n - 1] = temp
     return seq
