5ffbed40634fd9ed95a604b2d1fe5f1171ee435e
Andrew Szeto <andrew@jabagawee.com>
2012-09-29 19:34:11 -0700

    small tweaks thanks to @hughdbrown

--- algorithms/searching/binary_search.py
+++ algorithms/searching/binary_search.py
@@ -22,10 +22,10 @@
 
 def search(seq, key):
     lo = 0
-    hi = len(seq)
+    hi = len(seq) - 1
 
     while hi >= lo:
-        mid = (lo + hi) / 2
+        mid = (lo + (hi - lo)) / 2
         if seq[mid] < key:
             lo = mid + 1
         elif seq[mid] > key:
