7cefd214e4d7d834b9a86138abb48fc2861898c9
Scott Kirkpatrick <skirkpatrick4@mail.csuchico.edu>
2013-03-05 18:40:08 -0800

    Implement Mersenne Twister with Unit tests
    Unit tests compare results with those generated by the C++ STL
    Mersenne Twister mt19937 implementation.

---
+++ algorithms/random/mersenne_twister.py
@@ -0,0 +1,56 @@
+"""
+    mersenne_twister.py
+
+    Implementation of Mersenne Twister pseudo random number generator
+
+    Mersenne Twister Overview:
+    ---------------------------
+    Generates high quality pseudo random integers with a long period.
+    Used as the default random number generator for several
+    languages (including Python).
+
+    For a more technical overview, see the wikipedia entry.
+
+    Pseudocode: http://en.wikipedia.org/wiki/Mersenne_twister
+"""
+
+
+class MersenneTwister:
+    def __init__(self):
+        self.state = []
+        self.index = 0
+
+    def seed(self, seed):
+        """Initialize generator"""
+        self.state = []
+        self.index = 0
+        self.state.append(seed)
+        for i in range(1, 624):
+            n = (0x6c078965 * (self.state[i-1] ^ (self.state[i-1] >> 30)) + i)
+            n &= 0xffffffff
+            self.state.append(n)
+
+
+    def randint(self):
+        """Extract random number"""
+        if self.index == 0:
+            self.generate()
+            
+        y = self.state[self.index]
+        y ^= y >> 11
+        y ^= (y << 7) & 0x9d2c5680
+        y ^= (y << 15) & 0xefc60000
+        y ^= y >> 18
+        
+        self.index = (self.index + 1) % 624
+        return y
+
+
+    def generate(self):
+        """Generate 624 new random numbers"""
+        for i in range(624):
+            n = self.state[i] & 0x80000000
+            n += self.state[(i+1)%624] & 0x7fffffff
+            self.state[i] = self.state[(i+397)%624] ^ (n >> 1)
+            if n%2 != 0:
+                self.state[i] ^= 0x9908b0df
---
+++ algorithms/tests/test_random.py
@@ -0,0 +1,53 @@
+import unittest
+from ..random import mersenne_twister
+
+
+class TestMersenneTwister(unittest.TestCase):
+    """
+    Tests Mersenne Twister values for several seeds comparing against
+    expected values from C++ STL's Mersenne Twister implementation
+    """
+    
+    def test_mersenne_twister(self):
+        mt = mersenne_twister.MersenneTwister()
+
+        #Test seed 1
+        mt.seed(1)
+        self.expected = [1791095845, 4282876139, 3093770124,
+                         4005303368, 491263, 550290313, 1298508491,
+                         4290846341, 630311759, 1013994432]
+        self.results = []
+        for i in range(10):
+            self.results.append(mt.randint())
+        self.assertEqual(self.expected, self.results)
+
+        #Test seed 42
+        mt.seed(42)
+        self.expected = [1608637542, 3421126067, 4083286876,
+                         787846414, 3143890026, 3348747335,
+                         2571218620, 2563451924, 670094950, 1914837113]
+        self.results = []
+        for i in range(10):
+            self.results.append(mt.randint())
+        self.assertEqual(self.expected, self.results)
+
+        #Test seed 2147483647
+        mt.seed(2147483647)
+        self.expected = [1689602031, 3831148394, 2820341149,
+                         2744746572, 370616153, 3004629480,
+                         4141996784, 3942456616, 2667712047, 1179284407]
+        self.results = []
+        for i in range(10):
+            self.results.append(mt.randint())
+        self.assertEqual(self.expected, self.results)
+
+        #Test seed -1
+        #Hex is used to force 32-bit -1
+        mt.seed(0xffffffff)
+        self.expected = [419326371, 479346978, 3918654476,
+                         2416749639, 3388880820, 2260532800,
+                         3350089942, 3309765114, 77050329, 1217888032]
+        self.results = []
+        for i in range(10):
+            self.results.append(mt.randint())
+        self.assertEqual(self.expected, self.results)
