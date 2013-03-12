950d7d54f441fff65b7bf61cd2a284bb55247b5b
Conan Chou <conanchou1412@gmail.com>
2013-03-12 18:46:14 -0400

    tiny optimization

--- algorithms/sorting/heap_sort.py
+++ algorithms/sorting/heap_sort.py
@@ -36,14 +36,14 @@ def max_heapify(seq, i, n):
 
 def build_heap(seq):
     n = len(seq) - 1
-    for i in range(n, -1, -1):
+    for i in range(n/2, -1, -1):
         max_heapify(seq, i, n)
 
 
 def sort(seq):
     build_heap(seq)
     heap_size = len(seq) - 1
-    for x in range(heap_size, -1, -1):
+    for x in range(heap_size, 0, -1):
         seq[0], seq[x] = seq[x], seq[0]
         heap_size = heap_size - 1
         max_heapify(seq, 0, heap_size)
