f0ee0b92370f14f9580ef810fe9e0e18bdab6434
kabrapratik28 <kabrapratik28@gmail.com>
2014-07-16 01:19:39 +0530

    Data Structure Implemented 1.Stack 2.Queue

---
+++ algorithms/data_structure/stack.py
@@ -0,0 +1,30 @@
+"""
+   Stack data structure implemented:
+   --------------------------------
+   add : add element at last 
+   remove : remove element from last
+            return value 
+   is_empty : 1 value returned on empty
+              0 value returned on not empty
+   size : return size of stack
+
+   Time Complexity:  O(1)
+"""
+
+class stack : 
+    stack_list = []
+    def __init__(self):
+        self.stack_list = [] 
+    def add(self,value):
+        self.stack_list.append(value)
+    def remove(self):
+        return self.stack_list.pop()
+    def is_empty(self):
+        if not len(self.stack_list):
+            return 1 
+        else :
+            return 0
+    def size(self):
+        return len(self.stack_list)
+
+
---
+++ algorithms/tests/test_data_structure.py
@@ -0,0 +1,37 @@
+import unittest
+from ..data_structure import stack,queue
+
+class TestStack(unittest.TestCase):
+    """
+    Test Stack Implementation
+    """
+    def test_stack(self):
+        self.sta = stack.stack()
+        self.sta.add(5)
+        self.sta.add(8)
+        self.sta.add(10)
+        self.sta.add(2)
+
+        self.assertEqual(self.sta.remove(),2)
+        self.assertEqual(self.sta.is_empty(),0)
+        self.assertEqual(self.sta.size(),3)
+        
+class TestQueue(unittest.TestCase):
+    """
+    Test Queue Implementation
+    """
+    def test_queue(self):
+        self.que = queue.queue()
+        self.que.add(1)
+        self.que.add(2)
+        self.que.add(8)
+        self.que.add(5)
+        self.que.add(6)
+
+        self.assertEqual(self.que.remove(),1)
+        self.assertEqual(self.que.size(),4)
+        self.assertEqual(self.que.remove(),2)
+        self.assertEqual(self.que.remove(),8)
+        self.assertEqual(self.que.remove(),5)
+        self.assertEqual(self.que.remove(),6)
+        self.assertEqual(self.que.is_empty(),1)
