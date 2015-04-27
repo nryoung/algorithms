3f3e9b09656d537941ac01369d4b3a96c40c1be3
Truong Le <travistrle@gmail.com>
2015-04-27 14:10:51 -0400

    initialize for union find

---
+++ algorithms/data_structure/union_find.py
@@ -0,0 +1,58 @@
+"""
+    union_find.py
+    A Naive Implementation of union find data structure.
+    Union Find Overview:
+    ------------------------
+    A disjoint-set data structure, also called union-find data structure implements two functions:
+        union(A, B) - merge A's set with B's set
+        find(A) - finds what set A belongs to
+    Navie approach:
+        Find follows parent nodes until it reaches the root.
+        Union combines two trees into one by attaching the root of one to the root of the other
+    Time Complexity  :  O(N) (a highly unbalanced tree might be created)
+    Psuedo Code: http://en.wikipedia.org/wiki/Disjoint-set_data_structure
+"""
+class UnionFind:
+    def __init__(self, N):
+        if type(N) != int:
+            raise TypeError, "size must be integer"
+        if N < 0:
+            raise ValueError, "N is not a negative integer"
+        self.forests = []
+        self.N  = N
+        for i in range(0, N):
+            self.forests.append(i)
+
+    def make_set(self, x):
+        if type(x) != int:
+            raise TypeError, "x must be integer"
+        if x != self.N:
+            raise ValueError, "a new element must have index {0} since the total num of elements is {0}".format(self.N)
+        self.forests[x] = x
+
+    def union(self, x, y):
+        self.__validate_ele(x)
+        self.__validate_ele(y)
+        x_root = find(x)
+        y_root = find(y)
+        self.forests[x_root] = y_root
+
+    def find(self, x):
+        self.__validate_ele(x)
+        if self.forest[x] == x:
+            return x
+        else:
+            return find(x)
+
+    def is_connected(self, x, y):
+        self.__validate_ele(x)
+        self.__validate_ele(y)
+        if find(x) == find(y):
+            return True
+        return False
+
+    def __validate_ele(self, x):
+        if type(x) != int:
+            raise TypeError, "{0} is not an integer".format(x)
+        if x < 0 or x >= self.N:
+            raise ValueError, "{0} is not in [0,{1})".format(x, self.N)
