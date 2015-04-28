21c185548bc2232e53ea7188ed0cea32bac9562d
Truong Le <travistrle@gmail.com>
2015-04-27 22:52:36 -0400

    change union find class to make it follow oop encapsulation property

--- algorithms/data_structure/union_find.py
+++ algorithms/data_structure/union_find.py
@@ -18,32 +18,32 @@ class UnionFind:
             raise TypeError, "size must be integer"
         if N < 0:
             raise ValueError, "N cannot be a negative integer"
-        self.forests = []
-        self.N  = N
+        self.__parent = []
+        self.__N  = N
         for i in range(0, N):
-            self.forests.append(i)
+            self.__parent.append(i)
 
     def make_set(self, x):
         if type(x) != int:
             raise TypeError, "x must be integer"
-        if x != self.N:
-            raise ValueError, "a new element must have index {0} since the total num of elements is {0}".format(self.N)
-        self.forests.append(x)
-        self.N = self.N + 1
+        if x != self.__N:
+            raise ValueError, "a new element must have index {0} since the total num of elements is {0}".format(self.__N)
+        self.__parent.append(x)
+        self.__N = self.__N + 1
 
     def union(self, x, y):
         self.__validate_ele(x)
         self.__validate_ele(y)
         x_root = self.find(x)
         y_root = self.find(y)
-        self.forests[x_root] = y_root
+        self.__parent[x_root] = y_root
 
     def find(self, x):
         self.__validate_ele(x)
-        if self.forests[x] == x:
+        if self.__parent[x] == x:
             return x
         else:
-            return self.find(self.forests[x])
+            return self.find(self.__parent[x])
 
     def is_connected(self, x, y):
         self.__validate_ele(x)
@@ -53,5 +53,5 @@ class UnionFind:
     def __validate_ele(self, x):
         if type(x) != int:
             raise TypeError, "{0} is not an integer".format(x)
-        if x < 0 or x >= self.N:
-            raise ValueError, "{0} is not in [0,{1})".format(x, self.N)
+        if x < 0 or x >= self.__N:
+            raise ValueError, "{0} is not in [0,{1})".format(x, self.__N)
