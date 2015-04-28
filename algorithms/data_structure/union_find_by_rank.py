6c37ff47969c305e499d48473e9b2b7f45a22bdd
Truong Le <travistrle@gmail.com>
2015-04-27 22:57:23 -0400

    add union find by rank && union with path compression along with unit test

--- algorithms/data_structure/union_find_by_rank.py
+++ algorithms/data_structure/union_find_by_rank.py
@@ -1,5 +1,5 @@
 """
-    union_find.py
+    union_find_by_rank.py
     An implementation of union find by rank data structure.
     Union Find Overview:
     ------------------------
@@ -17,21 +17,21 @@ class UnionFindByRank:
             raise TypeError, "size must be integer"
         if N < 0:
             raise ValueError, "N cannot be a negative integer"
-        self.parent = []
-        self.rank = []
-        self.N  = N
+        self.__parent = []
+        self.__rank = []
+        self.__N  = N
         for i in range(0, N):
-            self.parent.append(i)
-            self.rank.append(0)
+            self.__parent.append(i)
+            self.__rank.append(0)
 
     def make_set(self, x):
         if type(x) != int:
             raise TypeError, "x must be integer"
-        if x != self.N:
-            raise ValueError, "a new element must have index {0} since the total num of elements is {0}".format(self.N)
-        self.parent.append(x)
-        self.rank.append(0)
-        self.N = self.N + 1
+        if x != self.__N:
+            raise ValueError, "a new element must have index {0} since the total num of elements is {0}".format(self.__N)
+        self.__parent.append(x)
+        self.__rank.append(0)
+        self.__N = self.__N + 1
 
     def union(self, x, y):
         self.__validate_ele(x)
@@ -41,20 +41,20 @@ class UnionFindByRank:
         if x_root == y_root:
             return
         # x and y are not already in same set. Merge them
-        if self.rank[x_root] < self.rank[y_root]:
-            self.parent[x_root] = y_root
-        elif self.rank[x_root] > self.rank[y_root]:
-            self.parent[y_root] = x_root
+        if self.__rank[x_root] < self.__rank[y_root]:
+            self.__parent[x_root] = y_root
+        elif self.__rank[x_root] > self.__rank[y_root]:
+            self.__parent[y_root] = x_root
         else:
-            self.parent[y_root] = x_root
-            self.rank[x_root] = self.rank[x_root] + 1
+            self.__parent[y_root] = x_root
+            self.__rank[x_root] = self.__rank[x_root] + 1
 
     def find(self, x):
         self.__validate_ele(x)
-        if self.parent[x] == x:
+        if self.__parent[x] == x:
             return x
         else:
-            return self.find(self.parent[x])
+            return self.find(self.__parent[x])
 
     def is_connected(self, x, y):
         self.__validate_ele(x)
@@ -64,6 +64,6 @@ class UnionFindByRank:
     def __validate_ele(self, x):
         if type(x) != int:
             raise TypeError, "{0} is not an integer".format(x)
-        if x < 0 or x >= self.N:
-            raise ValueError, "{0} is not in [0,{1})".format(x, self.N)
+        if x < 0 or x >= self.__N:
+            raise ValueError, "{0} is not in [0,{1})".format(x, self.__N)
 
--- algorithms/data_structure/union_find_with_path_compression.py
+++ algorithms/data_structure/union_find_with_path_compression.py
@@ -18,58 +18,62 @@ class UnionFindWithPathCompression:
             raise TypeError, "size must be integer"
         if N < 0:
             raise ValueError, "N cannot be a negative integer"
-        self.parent = []
-        self.rank = []
-        self.N  = N
+        self.__parent = []
+        self.__rank = []
+        self.__N  = N
         for i in range(0, N):
-            self.parent.append(i)
-            self.rank.append(0)
+            self.__parent.append(i)
+            self.__rank.append(0)
 
     def make_set(self, x):
         if type(x) != int:
             raise TypeError, "x must be integer"
-        if x != self.N:
-            raise ValueError, "a new element must have index {0} since the total num of elements is {0}".format(self.N)
-        self.parent.append(x)
-        self.rank.append(0)
-        self.N = self.N + 1
+        if x != self.__N:
+            raise ValueError, "a new element must have index {0} since the total num of elements is {0}".format(self.__N)
+        self.__parent.append(x)
+        self.__rank.append(0)
+        self.__N = self.__N + 1
 
     def union(self, x, y):
         self.__validate_ele(x)
         self.__validate_ele(y)
-        x_root = self.find(x)
-        y_root = self.find(y)
+        x_root = self.__find(x)
+        y_root = self.__find(y)
         if x_root == y_root:
             return
         # x and y are not already in same set. Merge them
-        if self.rank[x_root] < self.rank[y_root]:
-            self.parent[x_root] = y_root
-        elif self.rank[x_root] > self.rank[y_root]:
-            self.parent[y_root] = x_root
+        if self.__rank[x_root] < self.__rank[y_root]:
+            self.__parent[x_root] = y_root
+        elif self.__rank[x_root] > self.__rank[y_root]:
+            self.__parent[y_root] = x_root
         else:
-            self.parent[y_root] = x_root
-            self.rank[x_root] = self.rank[x_root] + 1
+            self.__parent[y_root] = x_root
+            self.__rank[x_root] = self.__rank[x_root] + 1
 
     def __find(self, x):
-        if self.parent[x] != x:
-            self.parent[x] = self.__find(self.parent[x])
-        return self.parent[x]
+        if self.__parent[x] != x:
+            self.__parent[x] = self.__find(self.__parent[x])
+        return self.__parent[x]
 
     def find(self, x):
         self.__validate_ele(x)
-        if self.parent[x] == x:
+        if self.__parent[x] == x:
             return x
         else:
-            return self.find(self.parent[x])
+            return self.find(self.__parent[x])
 
     def is_connected(self, x, y):
         self.__validate_ele(x)
         self.__validate_ele(y)
         return self.find(x) == self.find(y)
 
+    # use for unit testing check if the path is compressed
+    def parent(self, x):
+        return self.__parent[x]
+
     def __validate_ele(self, x):
         if type(x) != int:
             raise TypeError, "{0} is not an integer".format(x)
-        if x < 0 or x >= self.N:
-            raise ValueError, "{0} is not in [0,{1})".format(x, self.N)
+        if x < 0 or x >= self.__N:
+            raise ValueError, "{0} is not in [0,{1})".format(x, self.__N)
 
